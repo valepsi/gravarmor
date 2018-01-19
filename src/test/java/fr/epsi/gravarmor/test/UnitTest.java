87f5e82ead5bcffd5d82a00c1e55029513126f79
Valentin <valentin.lortet@epsi.fr>
2018-01-19 16:25:47 +0100

    Update test to junit5

--- src/test/java/fr/epsi/gravarmor/test/UnitTest.java
+++ src/test/java/fr/epsi/gravarmor/test/UnitTest.java
@@ -3,14 +3,16 @@ package fr.epsi.gravarmor.test;
 import fr.epsi.gravarmor.model.BoxType;
 import fr.epsi.gravarmor.model.LandBox;
 import fr.epsi.gravarmor.model.Team;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class UnitTest {
 
     @Test
+    @DisplayName("Test team name")
     public void testTeamName() {
 
         String teamName1 = "MaSuperTeam1";
@@ -21,16 +23,17 @@ public class UnitTest {
         Team team2 = new Team(teamName2, new ArrayList<>());
         Team team3 = new Team(teamName3, new ArrayList<>());
 
-        Assert.assertEquals(teamName1, team1.getName());
-        Assert.assertEquals(teamName2, team2.getName());
-        Assert.assertEquals(teamName3, team3.getName());
+        Assertions.assertEquals(teamName1, team1.getName());
+        Assertions.assertEquals(teamName2, team2.getName());
+        Assertions.assertEquals(teamName3, team3.getName());
 
-        Assert.assertNotEquals(team1.getName(), team2.getName());
-        Assert.assertNotEquals(team2.getName(), team3.getName());
-        Assert.assertNotEquals(team3.getName(), team1.getName());
+        Assertions.assertNotEquals(team1.getName(), team2.getName());
+        Assertions.assertNotEquals(team2.getName(), team3.getName());
+        Assertions.assertNotEquals(team3.getName(), team1.getName());
     }
 
     @Test
+    @DisplayName("Test box type")
     public void testBoxType() {
 
         BoxType boxType1 = BoxType.WATER;
@@ -41,12 +44,12 @@ public class UnitTest {
         LandBox landBox2 = new LandBox(boxType2);
         LandBox landBox3 = new LandBox(boxType3);
 
-        Assert.assertEquals(boxType1, landBox1.getType());
-        Assert.assertEquals(boxType2, landBox2.getType());
-        Assert.assertEquals(boxType3, landBox3.getType());
+        Assertions.assertEquals(boxType1, landBox1.getType());
+        Assertions.assertEquals(boxType2, landBox2.getType());
+        Assertions.assertEquals(boxType3, landBox3.getType());
 
-        Assert.assertNotEquals(landBox1.getType(), landBox2.getType());
-        Assert.assertNotEquals(landBox2.getType(), landBox3.getType());
-        Assert.assertNotEquals(landBox3.getType(), landBox1.getType());
+        Assertions.assertNotEquals(landBox1.getType(), landBox2.getType());
+        Assertions.assertNotEquals(landBox2.getType(), landBox3.getType());
+        Assertions.assertNotEquals(landBox3.getType(), landBox1.getType());
     }
 }
